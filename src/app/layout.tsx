b40544fdf62407b6a75172947054d9813a08beb2
Gustavobeckr <gustavobe0504@gmail.com>
2024-11-25 22:39:29 -0300

    fix: request http

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
       </head>
       <body className="bg-zinc-200 ">
         <NavBar />
